20d9d76519c09b121b37dc819d3df68c9663b6fd
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-04 11:54:02 +0200

    setup basic Appjs and Appscss

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import style from './App.scss'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className={style.content}>
+      </div>
+    )
+  }
+}
+
+export default App
