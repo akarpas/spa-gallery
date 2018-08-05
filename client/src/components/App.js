083c1d63ecf5c4c82ebf2346331ed85eb258a1ab
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-05 17:15:36 +0200

    create basic structure for main App component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from 'react'
+import Gallery from './organisms/Gallery'
+
 import style from './App.scss'
 
 class App extends React.Component {
+  componentWillMount() {
+    console.log("dispatch a default action")
+  }
+
   render() {
     return (
       <div className={style.content}>
+        <h2> Type anything you want to get photos of i.e. New York</h2>
+        <input className={style.input} placeholder='Search Flickr Images...' />
+        <Gallery />
       </div>
     )
   }
