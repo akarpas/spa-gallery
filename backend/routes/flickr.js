c1fedb6a56934f11f44c8e46135e9b6a1550c170
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-05 14:09:27 +0200

    setup basic flickr route

--- backend/routes/flickr.js
+++ backend/routes/flickr.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
+require('dotenv').config()
 
 
 router.post('/search', (req, res, next) => {
-  console.log(req.body.text)
-  
+  const { FLICKR_USER_ID, FLICKR_API_KEY } = process.env
+
+  const baseUrl = `https://api.flickr.com/services/rest/?method=`
+  const method = `flickr.photos.search`
+  const text = req.body.text.replace(' ','+')
+  const endUrl = `&api_key=${FLICKR_API_KEY}&text=${text}&per_page=20&sort=relevance&format=json&nojsoncallback=1`
+
+  return fetch(
+    `${baseUrl}${method}${endUrl}`,
+    {
+      method: 'get'
+    }
+  )
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    } else {
+      res.status(response.status).send()
+    }
+  })
+  .then(data => {
+    console.log(data)
+    res.status(200).send({
+      data
+    })
+  })
+
+
 })
 
 module.exports = router
