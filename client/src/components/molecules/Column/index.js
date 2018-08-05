8fc488d96589e401b1f0f5f3ac970d299eec0963
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-05 17:13:57 +0200

    create basic column component

---
+++ client/src/components/molecules/Column/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import style from './index.scss'
+
+class Column extends React.Component {
+  render() {
+    return (
+      <div className={style.column}>
+
+      </div>
+    )
+  }
+}
+
+export default Column
