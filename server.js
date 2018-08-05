26e39803e66b1bf194d003b520a1f95a8beaff70
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-05 12:23:05 +0200

    setup basic server file for backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const routes = require('./backend/routes')
+const bodyParser = require('body-parser')
+
+const DIST_DIR = path.join(__dirname, 'client/dist')
+const PORT = 5050
+const app = express()
+
+app.use(express.static(DIST_DIR))
+
+app.use(bodyParser.json({
+  limit: '2000kb'
+}))
+
+app.use('/api', routes)
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(DIST_DIR, 'index.html'))
+// })
+
+
+
+app.listen(PORT)
+
+console.log(`Listening on Port: ${PORT}`)
