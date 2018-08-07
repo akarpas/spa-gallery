4f4123cee9f349615eb186b2c754f43b18100497
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-07 19:17:23 +0200

    setup Columns component to render images with responsive css

--- client/src/components/molecules/Column/index.js
+++ client/src/components/molecules/Column/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
-
+import _ from 'lodash'
 import style from './index.scss'
 
 class Column extends React.Component {
   render() {
     return (
       <div className={style.column}>
-
+        {_.map(this.props.data, (image, index) => {
+          return (
+            <div key={`holder${index}`} className={style.container}>
+              <img key={`img${index}`} src={image.url}/>
+              <div className={style.center}>{image.username}</div>
+            </div>
+          ) 
+        })}
       </div>
     )
   }
