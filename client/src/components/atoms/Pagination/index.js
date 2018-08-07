6061239927e4c191a298ec04e214c0d159e8c4d3
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-07 19:19:52 +0200

    implement pagination atom to be used in Gallery

---
+++ client/src/components/atoms/Pagination/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {
+  NavLink
+} from 'react-router-dom'
+import style from './index.scss'
+
+class Pagination extends React.Component {
+  render() {
+    return (
+      <div className={style.pagination}>
+        <ul className={style.pages}>
+          <li className={style.separator}>
+            <NavLink className={style.nav} to={`/gallery/${1}`}>1</NavLink>
+          </li>
+          <li className={style.separator}>
+            <NavLink className={style.nav} to={`/gallery/${2}`}>2</NavLink>
+          </li>
+          <li className={style.separator}>
+            <NavLink className={style.nav} to={`/gallery/${3}`}>3</NavLink>
+          </li>
+          <li className={style.separator}>
+            <NavLink className={style.nav} to={`/gallery/${4}`}>4</NavLink>
+          </li>
+          <li>
+            <NavLink className={style.nav} to={`/gallery/${5}`}>5</NavLink>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Pagination
