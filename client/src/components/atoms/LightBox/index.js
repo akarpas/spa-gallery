1b932b06294105e5ea1e6cbae8b5cdfe31e0db14
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-07 22:00:22 +0200

    create lightbox component

---
+++ client/src/components/atoms/LightBox/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import _ from 'lodash'
+import { withRouter } from 'react-router-dom'
+import style from './index.scss'
+
+class LightBox extends React.Component {
+  render() {
+    const modal = _.get(this.props.location, 'state.modal')
+    console.warn(modal)
+    return (
+      <div className={style.overlay} id="closeA" onClick={(e) => this.props.toggleModal(e)}>
+        <div className={style.close} id="closeB" onClick={(e) => this.props.toggleModal(e)}> X - CLOSE - X</div>
+        <div className={style.imageContainer}>
+          <img className={style.image} src={this.props.image}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withRouter(LightBox)
