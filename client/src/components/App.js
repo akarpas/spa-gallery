51d779282a102d45691c419dbb89f0f0f4753c52
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-07 22:35:20 +0200

    add new route for Home

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,23 +2,24 @@
 import React from 'react'
 import {
   Route,
-  Router,
   HashRouter,
   Switch
 } from 'react-router-dom'
 import Gallery from './organisms/Gallery'
-import LightBox from './atoms/LightBox'
+import Home from './organisms/Home'
 
 import style from './App.scss'
 
 class App extends React.Component {
-  // <Route path="/gallery/:pageId/photo" component={LightBox}/>
   render() {
     return (
         <div className={style.mainContainer}>
           <div className={style.content}>
           <HashRouter>
-            <Route path="/gallery/:pageId" component={Gallery}/>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/gallery/:pageId" component={Gallery}/>
+            </Switch>
           </HashRouter>
           </div>
         </div>  
