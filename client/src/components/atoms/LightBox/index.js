60d69a6f391af4241f352c47697c7de31b95da54
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-08 21:11:32 +0200

    add more details to lightbox

--- client/src/components/atoms/LightBox/index.js
+++ client/src/components/atoms/LightBox/index.js
@@ -1,19 +1,31 @@
 import React from 'react'
-import _ from 'lodash'
-import { withRouter } from 'react-router-dom'
 import style from './index.scss'
 
 class LightBox extends React.Component {
+  ignore = (e) => {
+    e.stopPropagation()
+  }
   render() {
+    const { username, realName, title, profileUrl } = this.props.imageInfo
     return (
-      <div className={style.overlay} id="closeA" onClick={(e) => this.props.toggleModal(e)}>
-        <div className={style.close} id="closeB" onClick={(e) => this.props.toggleModal(e)}> X - CLOSE - X</div>
-        <div className={style.imageContainer}>
+      <div className={style.overlay} onClick={(e) => this.props.toggleModal(e)}>
+        <div className={style.close} onClick={(e) => this.props.toggleModal(e)}> X - CLOSE - X</div>
+        <div className={style.imageContainer} onClick={(e) => this.ignore(e)}>
           <img className={style.image} src={this.props.image}/>
+          {title &&
+            <div className={style.details}><strong>Title: </strong> {title} </div>}
+          {username &&
+            <div className={style.details}><strong>Username: </strong> {username} </div>}
+          {realName &&
+            <div className={style.details}><strong>Real Name: </strong> {realName} </div>}
+          {profileUrl &&
+            <div className={style.details}>
+              <strong>Profile: </strong> <a href={profileUrl} target="_blank"> Visit </a>
+            </div>}  
         </div>
       </div>
     )
   }
 }
 
-export default withRouter(LightBox)
+export default LightBox
