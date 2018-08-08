0344c2dd95e30cfba8370dd754bfa2523d2fdd31
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-08 19:01:07 +0200

    add cors to server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const path = require('path')
 const express = require('express')
 const routes = require('./backend/routes')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const DIST_DIR = path.join(__dirname, 'client/dist')
 const PORT = 5050
 const app = express()
 
+app.use(cors())
 app.use(express.static(DIST_DIR))
 
 app.use(bodyParser.json({
