8f19cf364f38e66dcc8637de53b0f592db5f1279
Andreas Karpasitis <karpasitis@outlook.com>
2018-08-08 20:02:44 +0200

    wrap fetch calls in async wrapper and change flow - add more data from user

--- backend/routes/flickr.js
+++ backend/routes/flickr.js
@@ -3,31 +3,32 @@ const router = express.Router()
 const fetch = require('node-fetch')
 const _ = require('lodash')
 const parser = require('../helpers/dataParser')
+const asyncWrapper = require('../utils/asyncWrapper')
 
 require('dotenv').config()
 const { FLICKR_API_KEY } = process.env
 
 callFlickr = (baseUrl,method,text,page) => {
-  const endUrl = method.includes('photos') ?
+    const endUrl = method.includes('photos') ?
     `&api_key=${FLICKR_API_KEY}&text=${text}&page=${page}&per_page=20&sort=relevance&format=json&nojsoncallback=1` :
     `&api_key=${FLICKR_API_KEY}&user_id=${text}&format=json&nojsoncallback=1`
 
-  return fetch(
-    `${baseUrl}${method}${endUrl}`,
-    {
-      method: 'get'
-    }
-  )
-  .then(response => {
-    if (response.ok) {
-      return response.json()
-    } else {
-      callFlickr(baseUrl,method,text,page)
-    }
-  })
-  .then(data => {
-    return data
-  })
+    return fetch(
+      `${baseUrl}${method}${endUrl}`,
+      {
+        method: 'get'
+      }
+    )
+    .then((response, error) => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        return Promise.reject(new Error(error))
+      }
+    })
+    .then(data => {
+      return Promise.resolve(data)
+    })
 }
 
 router.post('/search', async (req, res) => {
@@ -38,13 +39,33 @@ router.post('/search', async (req, res) => {
   const text = req.body.text.replace(' ','+')
   const page = req.body.page
 
-  const { photos } = await callFlickr(baseUrl,photosMethod,text,page)
+  const photosRequest = await asyncWrapper(callFlickr(baseUrl,photosMethod,text,page))
+
+  if (photosRequest.error) {
+    res.status(400)
+  }
+
+  const { photos } = photosRequest.data
   const photosUsernames = await Promise.all(
     _(photos.photo).map(async onePhoto => {
-      const user = await callFlickr(baseUrl,usersMethod,onePhoto.owner)
-      const username = user.person.username._content
+      const userRequest = await asyncWrapper(callFlickr(baseUrl,usersMethod,onePhoto.owner))
+
+      if (userRequest.error) {
+        res.status(400)
+      }
+      
+      const { person } = userRequest.data
+
+      const username = person.username._content
+      const realName = person.realname ? person.realname._content : ''
+      const profileUrl = person.profileurl ? person.profileurl._content : ''
+
       return {
-        username,
+        user: {
+          username,
+          realName,
+          profileUrl
+        },
         photo: onePhoto
       }
     })
